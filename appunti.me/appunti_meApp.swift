de95458f4f2c48d99aff53b4b97f7e4fd5cc77eb
Emiliano Maccaferri <inbox@emilianomaccaferri.com>
2022-07-04 20:49:49 +0200

    Initial Commit

---
+++ appunti.me/appunti_meApp.swift
@@ -0,0 +1,17 @@
+//
+//  appunti_meApp.swift
+//  appunti.me
+//
+//  Created by Emiliano Maccaferri on 04/07/22.
+//
+
+import SwiftUI
+
+@main
+struct appunti_meApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
